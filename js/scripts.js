f4194ccb95eff579646c8304f98a9bf67013d28e
Augusto Nascimento <augustomn91@hotmail.com>
2022-09-22 14:17:32 -0300

    adicionando informações de imc em array de objetos

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+/* tabela de dados imc */
+
+const data = [
+    {
+        minimo: 0,
+        maximo: 18.4,
+        classificacao: "Abaixo de 18,5",
+        informacao: "Abaixo do peso",
+        nivel: "0"
+    },
+    {
+        minimo: 18.5,
+        maximo: 24.9,
+        classificacao: "Entre 18,4 e 24,9",
+        informacao: "Normal",
+        nivel: "0"
+    },
+    {
+        minimo: 25,
+        maximo: 29.9,
+        classificacao: "Entre 25 e 29,9",
+        informacao: "Sobrepeso",
+        nivel: "I"
+    },
+    {
+        minimo: 30,
+        maximo: 39.9,
+        classificacao: "Entre 30 e 39,9",
+        informacao: "Obesidade",
+        nivel: "II"
+    },
+    {
+        minimo: 40,
+        maximo: 99,
+        classificacao: "Acima de 40",
+        informacao: "Obesidade grave",
+        nivel: "III"
+    },
+];
+
