9ff5fed31460b56b2f00d7aaff2c225f9c6bd1aa
Augusto Nascimento <augustomn91@hotmail.com>
2022-09-23 00:17:33 -0300

    adicionado informações na tela de resultado

--- js/scripts.js
+++ js/scripts.js
@@ -45,11 +45,15 @@ const height_input = document.querySelector("#height");
 const weight_input = document.querySelector("#weight");
 const calc_btn = document.querySelector("#calc-btn");
 const clear_btn = document.querySelector("#clear-btn");
+const screen_calc = document.querySelector("#calc-container");
 
 // elementos tela de resultado
 
 const table_results = document.querySelector("#table");
-
+const screen_result = document.querySelector("#result-container");
+const back_btn = document.querySelector("#back-btn");
+const result_imc = document.querySelector("#result-imc span");
+const classification_imc = document.querySelector("#classification-imc span");
 
 // funções
 
@@ -79,15 +83,70 @@ function createTable(data) {
 function clearInputs() {
     height_input.value = "";
     weight_input.value = "";
-}
+};
 
-// inicialização
+function validation(elem) {
+    return elem.replace(/[^0-9,]/g, "");
+};
 
-createTable(data);
+function hideScreen(){
+    screen_calc.classList.toggle("hide");
+    screen_result.classList.toggle("hide");
+};
+
+function calcImc(height, weight){
+    const result = (weight / (height * height)).toFixed(1);
+    return result;
+};
 
 // eventos
+
+calc_btn.addEventListener("click", function(e){
+    e.preventDefault();
+
+    const height = +height_input.value.replace(",", ".");
+    const weight = +weight_input.value.replace(",", ".");
+
+    if(!height || !weight) {return};
+
+    const imc = calcImc(height, weight);
+
+    let information;
+    
+    data.forEach((param) => {
+        if(imc >= param.minimo && imc <= param.maximo){
+            information = param.informacao;
+        };
+    });
+
+    if(!information){return};
+    
+
+    result_imc.innerText = imc;
+    classification_imc.innerText = information;
+
+    hideScreen();
+
+});
+
+[height_input, weight_input].forEach((ex) => {
+    ex.addEventListener("input", function(param){
+        const validation_input = validation(param.target.value);
+        param.target.value = validation_input;
+    });
+});
 
 clear_btn.addEventListener("click", (param) => {
     param.preventDefault();
     clearInputs();
 });
+
+back_btn.addEventListener("click", function(){
+    hideScreen();
+    clearInputs();
+});
+
+
+// inicialização
+
+createTable(data);
